ee82d2d240c33eecae39423e4a08b6b736ea9910
Егор <egorayudzhanov@gmail.com>
2021-04-12 21:09:14 +0300

    added missing images

--- src/test/java/ru/ayubdzhanov/javaquiz/JavaQuizApplicationTests.java
+++ src/test/java/ru/ayubdzhanov/javaquiz/JavaQuizApplicationTests.java
@@ -3,11 +3,19 @@ package ru.ayubdzhanov.javaquiz;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+
 class JavaQuizApplicationTests {
 
     @Test
     void contextLoads() {
+        LocalDateTime time = LocalDateTime.now().minusHours(1);
+        System.out.println(time);
+        System.out.println(time.toLocalDate());
+        System.out.println(Period.between(time.toLocalDate(),LocalDate.now()).getDays());
     }
+
 
 }
