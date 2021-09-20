3f83fd94b30d922de77a431b353d001ed0491285
Egor Ayubdzhanov <egor.ayubdzhanov@bsc-ideas.com>
2021-09-20 17:01:29 +0300

    Revert "glory"
    This reverts commit 8f90cffceab56d299d16b20d86fa74b197fe0e57.

--- src/main/java/ru/ayubdzhanov/javaquiz/JavaQuizApplication.java
+++ src/main/java/ru/ayubdzhanov/javaquiz/JavaQuizApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaQuizApplication {
 
     public static void main(String[] args) {
-        //glory to mankind
         SpringApplication.run(JavaQuizApplication.class, args);
     }
 }
